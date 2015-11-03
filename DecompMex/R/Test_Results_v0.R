a6fd740d40f3e76bfa3b748e7e0c7c2abde25b44
Jose Manuel Aburto <jm.aburto01@gmail.com>
2015-11-03 14:11:45 +0100

    HexMex experiment
    Hi, tim. Updated manuscript and figures. Some questions though

--- DecompMex/R/Test_Results_v0.R
+++ DecompMex/R/Test_Results_v0.R
@@ -1,12 +1,12 @@
 ############# Decomposition analyses for Mexican mortality 1990-2010
 ############# Results provided by Tim
-setwd("/Users/josemanuelaf/Desktop/JM&TIM_beta/Data")
+setwd("/Users/josemanuelaf/Desktop/JM&TIM_beta")
 library(latticeExtra)
 dir()
 ###### for ages 0 - 14 ##############################################################
 #####################################################################################
 ### For females
-DecomF0_14 <- local(get(load("ContribFemales0_14.Rdata")))
+DecomF0_14 <- local(get(load("Data/ContribFemales0_14.Rdata")))
 DF0_14 <- NULL
 require(reshape2)
 for( i in 1:21){
@@ -23,7 +23,7 @@ for( i in 1:21){
 ## Data frame with results for females
 
 ## For males
-DecomM0_14 <- local(get(load("ContribMales0_14.Rdata")))
+DecomM0_14 <- local(get(load("Data/ContribMales0_14.Rdata")))
 DM0_14 <- NULL
 for( i in 1:21){
   D        <- do.call(rbind.data.frame,DecomM0_14[[i]])
@@ -42,7 +42,7 @@ for( i in 1:21){
 ###### for ages 15 - 39 ##############################################################
 #####################################################################################
 ### For females
-DecomF15_39 <- local(get(load("ContribFemales15_39.Rdata")))
+DecomF15_39 <- local(get(load("Data/ContribFemales15_39.Rdata")))
 DF15_39 <- NULL
 for( i in 1:21){
   D        <- do.call(rbind.data.frame,DecomF15_39[[i]])
@@ -58,7 +58,7 @@ for( i in 1:21){
 ## Data frame with results for females
 
 ## For males
-DecomM15_39 <- local(get(load("ContribMales15_39.Rdata")))
+DecomM15_39 <- local(get(load("Data/ContribMales15_39.Rdata")))
 DM15_39 <- NULL
 for( i in 1:21){
   D        <- do.call(rbind.data.frame,DecomM15_39[[i]])
@@ -77,7 +77,7 @@ for( i in 1:21){
 ###### for ages 40 - 74 ##############################################################
 #####################################################################################
 ### For females
-DecomF40_74 <- local(get(load("ContribFemales40_74.Rdata")))
+DecomF40_74 <- local(get(load("Data/ContribFemales40_74.Rdata")))
 DF40_74 <- NULL
 for( i in 1:21){
   D        <- do.call(rbind.data.frame,DecomF40_74[[i]])
@@ -93,7 +93,7 @@ for( i in 1:21){
 ## Data frame with results for females
 
 ## For males
-DecomM40_74 <- local(get(load("ContribMales40_74.Rdata")))
+DecomM40_74 <- local(get(load("Data/ContribMales40_74.Rdata")))
 DM40_74 <- NULL
 for( i in 1:21){
   D        <- do.call(rbind.data.frame,DecomM40_74[[i]])
@@ -169,6 +169,29 @@ pdf(file="AMS_0_14.pdf",width=12,height=5,pointsize=4)
 print(F1)
 dev.off()
 
+##################################################################################
+
+F1.1 <-  useOuterStrips(xyplot(Contribution~Year|AMLabel+Sex,data=data1,groups=State,
+                             col=makeTransparent("black", alpha=150),ylim=c(0,1.6),
+                             par.settings=my.settings,type="l",
+                             scales=list(alternating=1,x=list(cex=.75,at=c(1995,2005)),
+                                         y=list(cex=.75,at=c(seq(0,2,.25)),alternating=3)),
+                             panel = function(x, y, ...){                        
+                               panel.abline(v=c(seq(1990,2010,5)),col='dark grey',lty=3)
+                               panel.abline(h=c(.2),col='red',lty=1)
+                               panel.abline(h=c(.5),col='red',lty=1)
+                               panel.abline(h=c(.1),col='blue',lty=1)
+                               panel.abline(h=c(seq(0,1.5,.25)),col='dark grey',lty=3)
+                               panel.xyplot(x, y,lty=1,...)
+                             }),strip.left=T)
+F1.1
+
+pdf(file="AM_0_14.pdf",width=12,height=5,pointsize=4)
+print(F1.1)
+dev.off()
+
+
+
 
 #######################################################################################################
 ################### from 15 to 39
@@ -179,7 +202,7 @@ DM15_39$Sex <- "Males"
 data2 <-rbind(DF15_39,DM15_39)
 
 F2 <-  useOuterStrips(xyplot(Contribution~Year|AMLabel+Sex,data=data2,groups=State,
-       col=makeTransparent("black", alpha=150),
+       col=makeTransparent("black", alpha=150),ylim=c(0,1.6),
        par.settings=my.settings,type="l",
        scales=list(alternating=1,x=list(cex=.75,at=c(1995,2005)),
                    y=list(cex=.75,at=c(seq(0,2,.25)),alternating=3)),
@@ -206,7 +229,7 @@ DM40_74$Sex <- "Males"
 data3 <-rbind(DF40_74,DM40_74)
 
 F3 <-  useOuterStrips(xyplot(Contribution~Year|AMLabel+Sex,data=data3,groups=State,
-                             col=makeTransparent("black", alpha=150),
+                             col=makeTransparent("black", alpha=150),ylim=c(0,1.6),
                              par.settings=my.settings,type="l",
                              scales=list(alternating=1,x=list(cex=.75,at=c(1995,2005)),
                                          y=list(cex=.75,at=c(seq(0,2,.25)),alternating=3)),
@@ -224,9 +247,46 @@ pdf(file="AM_40_74.pdf",width=12,height=5,pointsize=4)
 print(F3)
 dev.off()
 
-#require(gridExtra)
-#pdf(file="AMS_0_14.pdf",width=9,height=5,pointsize=4)
-#grid.arrange(F1, F2, ncol=2)
-#dev.off()
+require(gridExtra)
+pdf(file="TLE_Decomp.pdf",width=12,height=15,pointsize=2)
+grid.arrange(F1.1, F2,F3, nrow=3)
+dev.off()
 
+
+#################### Get into the Hex Mex
+source("R/HexTest.R")
+#data1 <-rbind(DF0_14,DM0_14)
+#data2 <-rbind(DF15_39,DM15_39)
+#data3 <-rbind(DF40_74,DM40_74)
+
+HexMex_Cause <- function(Data1,Year,Category,breaks){
+ data           <- as.data.frame(Data1)[Data1$Year == Year & Data1$AMCategory == Category,]
+ rownames(data) <- data$State
+ data           <- subset(data,select=c("State","Year","Contribution"))
+ return(HexMex(data,value.name = "Contribution",version =3,breaks=breaks))
+}
+
+require(gridExtra)
+pdf(file="HexMex_Homicide_15-39.pdf",width=6,height=4,pointsize=2)
+for(i in 1990:2010){
+  HexMex_Cause(Data1=DM15_39,Year=i,Category=7,breaks=NULL)  
+}
+dev.off()
+
+
+pdf(file="HexMex_maps.pdf",width=12,height=8,pointsize=2)
+par(mfrow=c(2,2))
+par(mar = c(1,1,1,1))
+HexMex_Cause(Data1=DM15_39,Year=1990,Category=7,breaks=NULL)  
+text(10,8,"main")
+HexMex_Cause(Data1=DM15_39,Year=1995,Category=7,breaks=NULL)  
+HexMex_Cause(Data1=DM15_39,Year=2000,Category=7,breaks=NULL)  
+HexMex_Cause(Data1=DM15_39,Year=2005,Category=7,breaks=NULL)  
+dev.off()
+
+pdf(file="HexMex_AMS_0-14.pdf",width=6,height=4,pointsize=2)
+for(i in 1990:2010){
+HexMex_Cause(Data1=DM0_14,Year=i,Category=1,breaks=c(.01,.02,.05,.06,.07,.08,.09,.1,.2,.25,.3,.35,.4,.5)) 
+}
+dev.off()
 
